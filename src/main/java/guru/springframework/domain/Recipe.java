bb040c996ad8ef073f79ca10a5df43a312fa8caa
sherfallak <sh.fallak@gmail.com>
2020-08-02 21:21:07 +0200

    Created Enum and add property in Recipe class.

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -16,7 +16,9 @@ public class Recipe {
     private String source;
     private String url;
     private String direction;
-    //private Difficulty difficulty;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @OneToOne(cascade = CascadeType.ALL, mappedBy = "recipe")
    private Set<Ingredient> ingredients;
 
@@ -112,5 +114,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
